e22bd854cf3b5487dd7d85def6f0308ea3a141f6
辻目 <781349304@qq.com>
2019-12-15 21:43:38 +0800

    feat: 添加错误统一处理

--- src/common/ErrorHandle.js
+++ src/common/ErrorHandle.js
@@ -1,3 +1,6 @@
+/**
+ * 错误处理中间件
+ */
 export default (ctx, next) => {
   return next().catch(err => {
     if (401 === err.status) {
@@ -7,7 +10,15 @@ export default (ctx, next) => {
         msg: 'Protected resource, use Authorization header to get access\n'
       };
     } else {
-      throw err;
+      ctx.status = err.status || 500;
+
+      ctx.body = Object.assign(
+        {
+          code: 500,
+          msg: err.message
+        },
+        process.env.NODE_ENV === 'development' ? { stack: err.stack } : {}
+      );
     }
   });
 };
